fb1b42451f616eec450cba3a708ea28bfceb541f
Fabrizio Barreto <barretofabrizio@gmail.com>
2021-05-28 18:46:48 -0300

    Añadido JWT

--- src/modelos/Usuario.js
+++ src/modelos/Usuario.js
@@ -3,6 +3,8 @@ const { Schema, model } = require('mongoose');
 const usuarioSchema = new Schema({
     email: String,
     pass: String
+}, {
+    timestamps: true
 });
 
 module.exports = model('Usuario', usuarioSchema);
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -3,12 +3,44 @@ const router = Router();
 
 const Usuario = require('../modelos/Usuario');
 
+const jwt = require('jsonwebtoken');
+
 router.get('/', (req, res) => res.send('Hola mundo dsafsaf'));
 
-router.post('/registro', (req, res) => {
+router.post('/registro', async (req, res) => {
 
-    console.log(req.body);
-    res.send('testing registro 2');
+    const { email, pass } = req.body;
+
+    const nuevoUsuario = new Usuario({
+        email: email,
+        pass: pass
+    });
+
+    await nuevoUsuario.save();
+
+    const token = jwt.sign({ _id: nuevoUsuario._id }, 'llaveSecreta');
+
+    return res.status(200).json({token});
+
+});
+
+router.post('/ingresar', async (req, res) => {
+
+    const { email, pass } = req.body;
+
+    const usuario = await Usuario.findOne({email});
+
+    if(!usuario){
+        return res.status(401).send('Datos incorrectos.');
+    }
+
+    if(usuario.pass !== pass) {
+        return res.status(401).send('Datos incorrectos. 9');
+    }
+
+    const token = jwt.sign({_id: usuario._id}, 'llaveSecreta');
+
+    return res.status(200).json({token});
 
 });
 
